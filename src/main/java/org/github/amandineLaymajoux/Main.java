881b853e7f8e14f9b9cbd8d494fff74dfcefc5de
Joshua SIX <joshua.six@etud.univ-jfc.fr>
2024-12-04 09:37:05 +0100

    kdo amandine pour ton livre d'or

--- src/main/java/org/github/amandineLaymajoux/Main.java
+++ src/main/java/org/github/amandineLaymajoux/Main.java
@@ -1,8 +1,31 @@
 package org.github.amandineLaymajoux;
 
-public class Main {
+import java.awt.*;
+
+public class Main extends Canvas {
     public static void main(String[] args) {
+
+        Main main = new Main();
+        Frame f = new Frame();
+        f.add(main);
+        f.setTitle("Coucou amandine");
+        f.setSize(100, 100);
         //commentaire
         System.out.println("Hello, World!");
+        System.out.println("On va afficher une page oki ?");
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        // Couleur du cœur
+        g.setColor(Color.RED);
+
+        // Points du cœur (forme approximative dans une fenêtre 100x100)
+        int[] xPoints = {50, 30, 10, 30, 50, 70, 90, 70};
+        int[] yPoints = {30, 10, 30, 60, 90, 60, 30, 10};
+
+        // Dessin du polygone du cœur
+        g.fillPolygon(xPoints, yPoints, xPoints.length);
     }
 }
