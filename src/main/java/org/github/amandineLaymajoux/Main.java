66179b85b415365a14ec1b1bd863ae19375ee1ae
Mani0726 <157810331+Mani0726@users.noreply.github.com>
2024-12-04 09:06:10 +0100

    1er Commit

---
+++ src/main/java/org/github/amandineLaymajoux/Main.java
@@ -0,0 +1,8 @@
+package org.github.amandineLaymajoux;
+
+public class Main {
+    public static void main(String[] args) {
+        //commentaire
+        System.out.println("Hello, World!");
+    }
+}
